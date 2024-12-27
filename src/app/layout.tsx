8835975edd5d4a161c1761b991d040a57554107d
VanCuong2002 <cuongcoca12345@gmail.com>
2024-12-27 09:45:31 +0700

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import Header from "~/components/layout/Header";
 import Footer from "~/components/layout/Footer";
 // import Heartfall from "~/components/shared/Heartfall";
 import { AppProvider } from "~/components/shared/AppProvider";
+import Script from "next/script";
 
 export default function RootLayout({
     children,
@@ -32,6 +33,22 @@ export default function RootLayout({
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+                />
+                <Script
+                    strategy="afterInteractive"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-7CNQP1SZ84"
+                />
+                <Script
+                    id="google-analytics"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7CNQP1SZ84');
+          `,
+                    }}
                 />
             </head>
             <body>
